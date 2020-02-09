81f0f5535a43e0671cf327d77d9fb8a0fb0ad5eb
DavidPalomeque <davidpalomeque2001@gmail.com>
2020-02-09 20:19:08 -0300

    first example done

---
+++ App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider, connect } from 'react-redux';
+import axios from 'axios';
+import axiosMiddleware from 'redux-axios-middleware';
+import reducer from './redux/reducer';
+import RepoList from './components/RepoList';
+
+const client = axios.create({
+  baseURL: 'https://api.github.com',
+  responseType: 'json'
+});
+
+const store = createStore(reducer, applyMiddleware(axiosMiddleware(client)));
+
+export default class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <View style={styles.container}>
+          <RepoList />
+        </View>
+      </Provider>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    marginTop: 50
+  }
+});
---
+++ redux/reducer.js
@@ -0,0 +1,38 @@
+// ducks-modular-redux pattern 
+// it is a way to organize the code
+// types , actions & reducer in a single file
+
+// TYPES
+export const GET_REPOS = 'my-awesome-app/repos/LOAD';
+export const GET_REPOS_SUCCESS = 'my-awesome-app/repos/LOAD_SUCCESS';
+export const GET_REPOS_FAIL = 'my-awesome-app/repos/LOAD_FAIL';
+
+// ACTIONS
+export function listRepos(user) {
+  return {
+    type: GET_REPOS,
+    payload: {
+      request: {
+        url: `/users/${user}/repos`
+      }
+    }
+  };
+}
+
+// REDUCER
+export default function reducer(state = { repos: [] }, action) {
+  switch (action.type) {
+    case GET_REPOS:
+      return { ...state, loading: true };
+    case GET_REPOS_SUCCESS:
+      return { ...state, loading: false, repos: action.payload.data };
+    case GET_REPOS_FAIL:
+      return {
+        ...state,
+        loading: false,
+        error: 'Error while fetching repositories'
+      };
+    default:
+      return state;
+  }
+}
